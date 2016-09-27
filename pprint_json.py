616f090bc7590da611b89b9f11fc66aabcd7c822
kajojify <rsamoilenko@mail.ru>
2016-09-27 12:36:56 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,29 @@ import json
 
 
 def load_data(filepath):
-    pass
+    """Декодирует json-данные в Python-объект. Возвращает список,
+    элементами которого являются словари(dicts), предоставляющие 
+    информацию об алкогольных магазинах Москвы."""
+
+    with open(filepath) as f:
+        alcohol_json = f.readline()
+        alcohol_data = json.loads(alcohol_json)
+    return alcohol_data
 
 
 def pretty_print_json(data):
-    pass
+    """Сериализует data в json-формат.
+    Выводит в консоль содержимое файла filepath в удобочитаемом
+    формате с отступом в 4 пробела."""
+
+    print(json.dumps(data, ensure_ascii=False, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input("Введите путь к файлу json --- ")
+    try:
+        alcohol_data = load_data(filepath)
+    except FileNotFoundError:
+        print("Нет такого файла или директории! Повторите ввод.")
+        exit()
+    pretty_print_json(alcohol_data)
